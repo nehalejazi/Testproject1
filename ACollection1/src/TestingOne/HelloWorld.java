62cbead69efcb3f96ad34d8c911c884fdb7d3c58
nehalejazi <40775600+nehalejazi@users.noreply.github.com>
2018-07-29 13:40:46 +0100

    Adding a minor change

--- ACollection1/src/TestingOne/HelloWorld.java
+++ ACollection1/src/TestingOne/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world line 1");
 		System.out.println("Hello world of second line 2");
+		System.out.println("Hello world of third line 3 from github");
 		ExampleClass1 abc = new ExampleClass1();
 		abc.sum();
 
