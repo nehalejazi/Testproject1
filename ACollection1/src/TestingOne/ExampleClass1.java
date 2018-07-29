9bc52899b03913a0fa680b5782501eb2cf6116bf
nehalejazi <mnejazi@gmail.com>
2018-07-29 13:37:22 +0100

    Second Commit

---
+++ ACollection1/src/TestingOne/ExampleClass1.java
@@ -0,0 +1,14 @@
+package TestingOne;
+
+public class ExampleClass1 {
+	
+	private int i =10;
+	private int j = 20;
+	
+	public void sum()
+	{
+		int sum1 = i+j;
+		System.out.println(sum1);
+	}
+
+}
--- ACollection1/src/TestingOne/HelloWorld.java
+++ ACollection1/src/TestingOne/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world line 1");
+		System.out.println("Hello world of second line 2");
+		ExampleClass1 abc = new ExampleClass1();
+		abc.sum();
 
 	}
 
